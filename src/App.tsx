c809276755a13cd16cc0930eda555f740d373e9e
Chris Cooper <chris.cooper@prettylittlething.com>
2024-07-25 15:39:48 +0100

    add some text to App

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import useCart from "./hooks/useCart";
 function App() {
   const { data, error, isLoading, handleUpdateItemQty } = useCart();
 
-  return <div className="bg-red-200"></div>;
+  return <div className="bg-red-200">
+    Cart items should go here
+  </div>;
 }
 
 export default App;
